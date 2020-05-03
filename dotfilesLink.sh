9bbe600dd2a3a3e8a13f765b03abd3c8ba5d5b93
Gart2357 <gart@outlook.jp>
2020-05-03 01:33:19 +0000

    emacs ln

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
