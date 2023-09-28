a1eb9156774b01842acf1102c1b6a6e91f5fd697
gaato <g.furuya@gaato.net>
2023-09-28 14:53:36 +0900

    Update setup script

--- dotfilesLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.nanorc ~/.nanorc
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/wezterm ~/.config/wezterm
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+# Function to create symbolic links
+create_symlink() {
+  src="$1"
+  dest="$2"
+
+  # Create parent directory if it doesn't exist
+  parent_dir=$(dirname "$dest")
+  mkdir -p "$parent_dir"
+
+  # Confirm if the file already exists
+  if [[ -e "$dest" ]]; then
+    read "yn?$dest already exists. Overwrite? [y/N] "
+    case $yn in
+      [Yy]* ) ;;
+      * ) return;;
+    esac
+  fi
+
+  # Create the symbolic link
+  ln -sf "$src" "$dest"
+}
+
+# List of files and directories to include
+include=(".zshrc" ".vimrc" ".nanorc" "nvim/init.lua" "wezterm/wezterm.lua")
+
+# Path to the dotfiles directory
+dotfiles_dir="$PWD"
+
+# Create symbolic links for specified files and directories in the dotfiles directory
+for file in $include; do
+  src="$dotfiles_dir/$file"
+  dest="$HOME/$file"
+
+  # Check if the source file exists
+  if [[ ! -e "$src" ]]; then
+    echo "$src does not exist. Skipping."
+    continue
+  fi
+
+  create_symlink "$src" "$dest"
+done
