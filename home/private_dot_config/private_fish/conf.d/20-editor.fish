e9b463175692597adfbd00cf9750743923760a6c
Gakuto Furuya <g.furuya@gaato.net>
2026-07-18 17:31:59 +0900

    Set EDITOR/VISUAL as fish lists so $EDITOR works interactively

--- home/private_dot_config/private_fish/conf.d/20-editor.fish
+++ home/private_dot_config/private_fish/conf.d/20-editor.fish
@@ -1,6 +1,8 @@
 # エディタは daemon 稼働中の Emacs に接続する (herdr ペインでは -t でTUI)
-set -gx EDITOR "emacsclient -t"
-set -gx VISUAL "emacsclient -c"
+# クォートせずリストで持つ: fish で `$EDITOR file` と打ったときに引数分割されるように
+# (子プロセスへはスペース結合で export されるので sh 系からも従来通り動く)
+set -gx EDITOR emacsclient -t
+set -gx VISUAL emacsclient -c
 
 abbr -a e "emacsclient -t"
 abbr -a ec "emacsclient -c"
