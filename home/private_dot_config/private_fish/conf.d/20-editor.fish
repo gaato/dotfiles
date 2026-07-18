7363c1b8e14ec30187e55ba91067b4ba9237f610
Gakuto Furuya <g.furuya@gaato.net>
2026-07-19 01:34:26 +0900

    Switch EDITOR to nano, keep Emacs via e/ec abbrs

--- home/private_dot_config/private_fish/conf.d/20-editor.fish
+++ home/private_dot_config/private_fish/conf.d/20-editor.fish
@@ -1,8 +1,6 @@
-# エディタは daemon 稼働中の Emacs に接続する (herdr ペインでは -t でTUI)
-# クォートせずリストで持つ: fish で `$EDITOR file` と打ったときに引数分割されるように
-# (子プロセスへはスペース結合で export されるので sh 系からも従来通り動く)
-set -gx EDITOR emacsclient -t
-set -gx VISUAL emacsclient -c
+# さくっと編集は nano、本気の編集は abbr の e/ec から Emacs daemon へ
+# VISUAL は設定しない (git 等が VISUAL を優先して nano が効かなくなるため)
+set -gx EDITOR nano
 
 abbr -a e "emacsclient -t"
 abbr -a ec "emacsclient -c"
